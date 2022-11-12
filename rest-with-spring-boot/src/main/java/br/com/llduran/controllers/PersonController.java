1707f74cc2c05e55a95f9c38d3c650e58f906ab7
Lindineu Lopes Duran <37485332+LindineuDuran@users.noreply.github.com>
2022-11-12 17:45:54 -0300

    Implementing Content Negotiation with JSON and XML

--- rest-with-spring-boot/src/main/java/br/com/llduran/controllers/PersonController.java
+++ rest-with-spring-boot/src/main/java/br/com/llduran/controllers/PersonController.java
@@ -2,8 +2,8 @@ package br.com.llduran.controllers;
 
 import br.com.llduran.data.vo.v1.PersonVO;
 import br.com.llduran.services.PersonServices;
+import br.com.llduran.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,29 +16,22 @@ public class PersonController
 	@Autowired
 	private PersonServices service;
 
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<PersonVO> findAll()
-	{
-		return service.findAll();
-	}
+	@GetMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
+	public List<PersonVO> findAll() { return service.findAll(); }
 
-	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public PersonVO findById(@PathVariable(value = "id") Long id)
-	{
-		return service.findById(id);
-	}
+	@GetMapping(value = "/{id}",
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
+	public PersonVO findById(@PathVariable(value = "id") Long id) { return service.findById(id); }
 
-	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public PersonVO create(@RequestBody PersonVO person)
-	{
-		return service.create(person);
-	}
+	@PostMapping(
+			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  },
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
+	public PersonVO create(@RequestBody PersonVO person) { return service.create(person); }
 
-	@PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public PersonVO update(@PathVariable(value = "id") Long id, @RequestBody PersonVO person)
-	{
-		return service.update(id, person);
-	}
+	@PutMapping(
+			consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  },
+			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
+	public PersonVO update(@PathVariable(value = "id") Long id, @RequestBody PersonVO person) { return service.update(id, person); }
 
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id)
